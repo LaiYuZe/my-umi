3376401163355f108093afc845e72883ef4fd90a
laiyuze <laiyuze@163.com>
2021-08-22 17:41:16 +0800

    feat: change hashroute

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,4 @@
-import { defineConfig, Router } from 'umi';
+import { defineConfig } from 'umi';
 
 export default defineConfig({
   nodeModulesTransform: {
@@ -11,4 +11,7 @@ export default defineConfig({
   // ],
   // ssr: {
   // },
+  history: {
+    type: 'hash',
+  },
 });
