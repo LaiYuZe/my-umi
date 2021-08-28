f013758cf58c147be9e27c838a923fe88f55874b
laiyuze <laiyuze@163.com>
2021-08-28 22:40:25 +0800

    feat: test umi git hub actions

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ export default function IndexPage() {
   const [data] = useState([3, 2, 1]);
   return (
     <div>
-      123
+      12345
       {data.map((item) => {
         return <h1 key={item}>home {item}</h1>;
       })}
